0a7283d3890e3ca3f1a1651053bd74e334b73d8c
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2020-01-14 01:46:10 +0700

    edit

--- application/models/lecturers_model.php
+++ application/models/lecturers_model.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class lecturers_model extends CI_Model{
+    class Lecturers_model extends CI_Model{
 
         private $tbl_name = "lecturers";
         const num = 0;
@@ -278,6 +278,26 @@
         //     return $result->result();
         // }
 
+        function import_lecturer($data){
+            $this->db->trans_begin();
 
+                foreach ($data['user'] as $i => $v) {
+                    $this->db->insert('users', $v);
+                    $user_id = $this->db->insert_id();
+                    $data['lecturer'][$i]['user_id'] = $user_id;
+                    $this->db->insert('lecturers',  $data['lecturer'][$i]);
+                }
+
+            if ($this->db->trans_status() === FALSE)
+            {
+                $this->db->trans_rollback();
+                return false;
+            }
+            else
+            {
+                $this->db->trans_commit();
+                return true;
+            }
+        }
     }
 ?>
